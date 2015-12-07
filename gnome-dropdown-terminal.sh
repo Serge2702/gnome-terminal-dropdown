627a44d67b6a5ca556325f1cdf70990e5f5dae13
Serge2702 <sergiogmv2702@gmail.com>
2015-12-07 16:06:35 -0600

    Added xdotool windowactivate
    Sometimes the window got mapped but wasn't activated. This change corrected that
    issue. But now the following message appears:
    "XGetWindowProperty[_NET_WM_DESKTOP] failed (code=1)"

--- gnome-dropdown-terminal.sh
+++ gnome-dropdown-terminal.sh
@@ -5,12 +5,13 @@ if [ -z "$wid" ]; then
     xdotool set_window --class dropdown "$wid" windowmove "$wid" 0 0 windowsize "$wid" x 50%
     wmctrl -i -r "$wid" -b add,maximized_horz,above
     wmctrl -i -r "$wid" -b add,sticky
-
+    xdotool windowactivate "$wid"
 else
     if [ -z "$(xdotool search --onlyvisible --class dropdown 2>/dev/null)" ]; then
         xdotool  windowmap "$wid" windowmove "$wid" 0 0  windowsize "$wid" x 50%
         wmctrl -i -r "$wid" -b add,maximized_horz,above
         wmctrl -i -r "$wid" -b add,sticky
+        xdotool windowactivate "$wid"
     else
         xdotool windowunmap "$wid"
     fi
