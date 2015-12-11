a4527261068f264c0f5d35587fb80ef29c03eee2
Serge2702 <sergiogmv2702@gmail.com>
2015-12-10 19:21:11 -0600

    Revert "Now it checks if the window still exists"
    This reverts commit 2111fbb680dd1ad0cd84871985f345980572b0fe.
    
    It's not really necessary to check if the window exists.

--- gnome-dropdown-terminal.sh
+++ gnome-dropdown-terminal.sh
@@ -13,19 +13,7 @@ if [ -z "$wid" ]; then
 else
     if [ -e "/tmp/.terminal-visible" ]; then
         xdotool windowunmap "$wid"
-        if [ "$?" -ne 0 ]
-        then
-            gnome-terminal -e 'conky -c /home/sergio/.conkyrc_ncurses'
-            wid=$(xdotool search --class gnome-terminal | tail -n1)
-            xdotool set_window --class dropdown "$wid" windowmove "$wid" 0 0 windowsize "$wid" x 50%
-            wmctrl -i -r "$wid" -b add,maximized_horz,above
-            wmctrl -i -r "$wid" -b add,sticky
-            xdotool windowactivate "$wid"
-            touch /tmp/.terminal-visible
-            echo $wid > /tmp/.terminal-wid
-        else
-            rm /tmp/.terminal-visible
-        fi
+        rm /tmp/.terminal-visible
     else
         xdotool  windowmap "$wid" windowmove "$wid" 0 0  windowsize "$wid" x 50%
         wmctrl -i -r "$wid" -b add,maximized_horz,above
